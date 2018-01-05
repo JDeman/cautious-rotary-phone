f15734f2760bfd99f88cf70197ae8e7d52f29986
J.Deman <juliendeman.nc@gmail.com>
2018-01-05 17:50:30 +0100

    program now uses a boat too

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -7,9 +7,12 @@ namespace ConsoleApplication
         private static void Main(string[] args)
         {
             IVehicle ride = new Car();
+            IVehicle boat = new Boat();
             Console.WriteLine("Type of ride object is => " + ride.Brand);
             if (ride.Brand == null) Console.WriteLine("Car has no Brand wtf");
             ride.PersonCap = 3;
+            Console.WriteLine("boat is a " + boat.Brand + " " + boat.Model + 
+                              " and can hold " + boat.PersonCap + " dudes");
             Console.WriteLine("Hello Dudes, Ya'll want a ride ?");
 
             ride.LoadPeople(2);
@@ -17,7 +20,15 @@ namespace ConsoleApplication
             ride.TurnLeft();
             ride.TurnRight();
             ride.Explode();
-            ride.Yolo();
+
+            Console.WriteLine("**********************");
+            
+            boat.LoadPeople(2);
+            boat.LoadPeople(6);
+            boat.TurnLeft();
+            boat.TurnRight();
+            boat.Explode();
+           
 
             Console.WriteLine("End.");
         }
