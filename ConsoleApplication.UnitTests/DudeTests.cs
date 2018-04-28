c89ac0ed28dfffe74bde6db1ca89c280212818c7
Joe <nullreferenceexception@protonmail.com>
2018-04-28 23:50:43 +0200

    More tests on DudeTests class + tweaked .gitignore

--- ConsoleApplication.UnitTests/DudeTests.cs
+++ ConsoleApplication.UnitTests/DudeTests.cs
@@ -5,45 +5,34 @@ namespace ConsoleApplication.UnitTests
     
     public class DudeTests
     {
-        [Fact]
-        public void WeightCheck_Case_GoodShape()
+//        [Fact]
+//        public void WeightCheck_Case_GoodShape()
+//        {
+//            var dude = new Dude{Name = "Jack", Age = 32, Gender = Genders.Man, Weight = 78};
+//
+//            var str = dude.WeightCheck();
+//
+//            Assert.Equal(str, "Jack is in good shape.");
+//
+//        }
+
+
+        [Theory]
+        [InlineData("Jim",0," get your ass to KFC right now!")]
+        [InlineData("Bob",25," get your ass to KFC right now!")]
+        [InlineData("Rita",59," get your ass to KFC right now!")]
+        [InlineData("Mike",70," is in good shape.")]
+        [InlineData("Joe",79," is in good shape.")]
+        [InlineData("Travis",102," needs to drop some pounds.")]
+        [InlineData("Jake",120," needs to drop some pounds.")]
+        [InlineData("Rudolph",123," is bigger than Mobby Dick.")]
+        [InlineData("Rudolph",345," is bigger than Mobby Dick.")]
+        [InlineData("Rudolph",1230," is bigger than Mobby Dick.")]
+        public void WeightCheck_Returns_Propper_Sentence(string name, int weight, string result)
         {
-            var dude = new Dude{Name = "Jack", Age = 32, Gender = Genders.Man, Weight = 78};
-
+            var dude = new Dude{Name = name, Weight = weight};
             var str = dude.WeightCheck();
-
-            Assert.Equal(str, "Jack is in good shape.");
-
-        }
-        
-        [Fact]
-        public void WeightCheck_Case_HugeWeight()
-        {
-            var dude = new Dude{Name = "Paula", Age = 54, Gender = Genders.Trans, Weight = 154};
-
-            var str = dude.WeightCheck();
-
-            Assert.Equal(str, "Paula is bigger than Mobby Dick.");
-        }
-        
-        [Fact]
-        public void WeightCheck_Case_Overweight()
-        {
-            var dude = new Dude{Name = "Rita", Age = 72, Gender = Genders.Woman, Weight = 116};
-
-            var str = dude.WeightCheck();
-
-            Assert.Equal(str, "Rita needs to drop some pounds.");
-        }
-        
-        [Fact]
-        public void WeightCheck_Case_Skinny()
-        {
-            var dude = new Dude{Name = "Bob", Age = 72, Gender = Genders.Man, Weight = 61};
-
-            var str = dude.WeightCheck();
-
-            Assert.Equal(str, "Bob get your ass to KFC right now!");
+            Assert.Equal(str, name + result);
         }
     }
 }
